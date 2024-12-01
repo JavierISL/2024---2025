63739d82ece4a972718d976bcca343897430dea3
Javier Ignacio Sánchez <149967485+JavierISL@users.noreply.github.com>
2024-12-01 19:56:33 +0100

    PROGRAMACION 2024/2025

---
+++ TEMA 1/hola mundo/src/Entrada.java
@@ -0,0 +1,8 @@
+public class Entrada {
+
+    public static void main(String[]args) {
+System.out.println("Este es mi primer programa de Java en 2025");
+System.out.println("Esta en mi segunda linea escrita en JAVA");
+    }
+
+}
---
+++ TEMA 1/hola mundo/src/EntradaFigura.java
@@ -0,0 +1,10 @@
+import com.sun.tools.javac.Main;
+
+public class EntradaFigura {
+
+    public static void main(String[]args) {
+        int radio = 4;
+                double area = 2*Math.PI*(radio*radio);
+        System.out.println("El area de un circulo es "+area);
+    }
+}
